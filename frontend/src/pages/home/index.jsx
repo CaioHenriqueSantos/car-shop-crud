bf79c395950ef5dd6b54706d612d61a6eab10e46
Caio Santos <caiohenriquedassantos@gmail.com>
2024-11-28 17:59:23 -0300

    first commit

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,11 @@
+import MyInput from "../../components/my-input";
+import "./home.css";
+
+export default function Home() {
+  return (
+    <div>
+      <h2>Home</h2>
+      <MyInput />
+    </div>
+  );
+}
